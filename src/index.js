3aac0b8fa9c6a81e70bec5b016547544c89011d3
digksskawk01 <digksskawk01@gmail.com>
2022-11-16 16:36:14 +0900

    work

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducers/app';
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://349dead2b9544a21bb81393a42afbc7e@ops-sentry.knowre.com/35",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const store = createStore(
   reducer,
   applyMiddleware(logger)
