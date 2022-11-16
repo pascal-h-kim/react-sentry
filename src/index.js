92a2776af331a7565247973336b8af34adbe61d3
pascal-h-kim <hyun.kim@knowre.com>
2022-11-16 17:33:44 +0900

    Work

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducers/app';
 
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
+import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
   dsn: "https://349dead2b9544a21bb81393a42afbc7e@ops-sentry.knowre.com/35",
-  integrations: [new BrowserTracing()],
+  integrations: [new Integrations.BrowserTracing()],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
